4b7bea13998d244d8c7ab6ee2059e3ba7cb4743c
Lalit gurjar <gujarlalit79@gmail.com>
2026-07-08 15:27:54 +0530

    refactor: remove unused Telegram mode files and clean up CLI mode

--- modes/cli.ts
+++ modes/cli.ts
@@ -1,8 +1,6 @@
-import chalk from "chalk";
 import { runChatUI } from "../tui/chat.js";
 
 async function runCliMode() {
-  console.log(chalk.green("You chose CLI Mode!"));
   await runChatUI();
 }
 
