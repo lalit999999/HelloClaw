2ce5d58828f9e3db72d6e225a5ce44d3370bf7e8
Lalit gurjar <gujarlalit79@gmail.com>
2026-07-08 15:35:26 +0530

    refactor: simplify chat UI resize handling and cleanup

--- tui/chat.tsx
+++ tui/chat.tsx
@@ -3,28 +3,15 @@ import { render } from "ink";
 import { Root } from "./app/Root.js";
 import { enterFullScreen, installExitHandlers, restoreTerminal } from "./terminal.js";
 
-const RESIZE_CLEAR_DEBOUNCE_MS = 80;
-
 export async function runChatUI(): Promise<void> {
   enterFullScreen();
   installExitHandlers();
 
-  const instance = render(<Root />);
-
-  let resizeTimer: NodeJS.Timeout | undefined;
-  const onResize = () => {
-    if (resizeTimer) clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      instance.clear();
-    }, RESIZE_CLEAR_DEBOUNCE_MS);
-  };
-  process.stdout.on("resize", onResize);
+  const { waitUntilExit } = render(<Root />);
 
   try {
-    await instance.waitUntilExit();
+    await waitUntilExit();
   } finally {
-    process.stdout.off("resize", onResize);
-    if (resizeTimer) clearTimeout(resizeTimer);
     restoreTerminal();
   }
 }
